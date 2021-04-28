7914c8413ede7db336bd97b83ddbcc7f4b4744c1
HyunSeung Lee <libarch010@gmail.com>
2021-04-28 13:31:36 -0500

    refactor: Reading CITY_DATA file of load_data function

--- bikeshare.py
+++ bikeshare.py
@@ -41,15 +41,15 @@ def get_filters():
     # get user input for day of week (all, monday, tuesday, ... sunday)
     while True:
         day = str(input('\nEnter the day you are interested in: (all, monday, tuesday, ... sunday)')).lower()
-        if day in day_name : 
+        if day in day_name :
             print('Thank you')
             break
         elif day == 'all':
             print('Thank you')
             break
-        else : 
+        else :
             print('That\'s not a valid input. Please enter the full name of the days(ex. monday or Monday).')
-            
+
     print('-'*40)
     return city, month, day
 
@@ -66,37 +66,36 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
 # load data file into a dataframe
-    df = pd.read_csv(CITY_DATA[city])
+    with pd.read_csv(CITY_DATA[city]) as df:
+        # convert the Start Time column to datetime
+        df['Start Time'] = pd.to_datetime(df['Start Time'])
 
-    # convert the Start Time column to datetime
-    df['Start Time'] = pd.to_datetime(df['Start Time'])
-
-    # extract month and day of week from Start Time to create new columns
-    df['month'] = df['Start Time'].dt.month
-    df['day_of_week'] = df['Start Time'].dt.weekday_name
+        # extract month and day of week from Start Time to create new columns
+        df['month'] = df['Start Time'].dt.month
+        df['day_of_week'] = df['Start Time'].dt.weekday_name
 
     # filter by month if applicable
-    if month != 'all':
-        # use the index of the months list to get the corresponding int
-        months = ['january', 'february', 'march', 'april', 'may', 'june']
-        month = months.index(month) + 1
+        if month != 'all':
+            # use the index of the months list to get the corresponding int
+            months = ['january', 'february', 'march', 'april', 'may', 'june']
+            month = months.index(month) + 1
 
         # filter by month to create the new dataframe
-        df = df[df['month'] == month]
+            df = df[df['month'] == month]
 
     # filter by day of week if applicable
-    if day != 'all':
-        # filter by day of week to create the new dataframe
-        df = df[df['day_of_week'] == day.title()]
+        if day != 'all':
+            # filter by day of week to create the new dataframe
+            df = df[df['day_of_week'] == day.title()]
 
-    return df
+        return df
 
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
-    while True: 
+    while True:
         try:
             # display the most common month
             common_month = df['month'].mode()[0]
@@ -115,7 +114,7 @@ def time_stats(df):
             print('Most Common day:', common_day_of_week)
             print('Most Common Start Hour:', common_hour)
             break
-        except KeyError: 
+        except KeyError:
             print('There is no data for this section.')
             break
     print('-'*40)
@@ -129,8 +128,8 @@ def station_stats(df):
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
-    while True: 
-        try:            
+    while True:
+        try:
             # display most commonly used start station
             common_start_station = df['Start Station'].value_counts().keys().tolist()[:1]
             # display most commonly used end station
@@ -142,10 +141,10 @@ def station_stats(df):
             print('Most Common end station: ', common_end_station)
             print('Most Common route: ', common_path)
             break
-        except KeyError: 
+        except KeyError:
             print('There is no data for this section.')
             break
-            
+
     print("\nThis took %s seconds." % (round(time.time() - start_time, 2)))
     print('-'*40)
 
@@ -156,7 +155,7 @@ def trip_duration_stats(df):
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
-    while True: 
+    while True:
         try:
             # display total travel time
             total_travel = df['Trip Duration'].sum()
@@ -164,13 +163,13 @@ def trip_duration_stats(df):
             print('Total travel time: ', total_travel)
             # display mean travel time
             mean_travel = time_convert(df['Trip Duration'].mean())
-            
+
             print('Mean travel time: ', mean_travel)
             break
         except KeyError:
             print('There is no data for this trip_duration_stats section.')
             break
-                        
+
     print("\nThis took %s seconds." % (round(time.time() - start_time, 2)))
     print('-'*40)
 
@@ -180,13 +179,13 @@ def user_stats(df):
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
-    
-    while True: 
+
+    while True:
         try:
             # Display counts of user types
             user_types = df['User Type'].value_counts()
             print('Number of users: ', user_types)
-            
+
             # Display counts of gender
             gender = df['Gender'].value_counts()
             #print('Gender: ', gender)
@@ -202,14 +201,14 @@ def user_stats(df):
 
     print("\nThis took %s seconds." % (round(time.time() - start_time, 2)))
     print('-'*40)
-    
+
 def display_raw_data(df):
     """ Presenting options for displaying raw data """
     i = 0
-    raw = input("Do you wish to view the data for this result? Enter yes or no...\n").lower() 
+    raw = input("Do you wish to view the data for this result? Enter yes or no...\n").lower()
     pd.set_option('display.max_columns',200)
 
-    while True:            
+    while True:
         if raw == 'no':
             break
         elif raw == 'yes':
@@ -225,7 +224,7 @@ def time_convert(seconds):
     seconds %= 3600
     minutes = seconds // 60
     seconds %= 60
-      
+
     return "%d hours %02d minutes %02d seconds" % (hour, minutes, seconds)
 
 def main():
